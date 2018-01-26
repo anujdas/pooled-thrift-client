89da238f58e830019a0b7aa87b86771d46316ab4
Anuj Das <anuj.das@brigade.com>
2018-01-25 17:07:50 -0800

    Revert "Rework callbacks to remove edge cases"
    This reverts commit 2b0e0797d2a16cf9f8a8618fa2a1dc24d6aa8a0f.

--- src/index.js
+++ src/index.js
@@ -6,47 +6,6 @@ const {
   ConnectionClosedError,
 } = require('./errors');
 
-/* Callback handlers */
-
-/**
- * Wraps a connection in event callbacks suitable for Promise use
- *
- * @param {Connection} connection A Thrift connection
- * @param {function} reject A Promise's reject function
- *
- * @return {Object} Three callbacks: onTimeout, onClose, onError
- */
-const attachCallbacks = (connection, reject) => {
-  const onTimeout = () => {
-    connection.alive = false;
-    reject(new ConnectionTimeoutError());
-  };
-  const onClose = () => {
-    connection.alive = false;
-    reject(new ConnectionClosedError());
-  };
-  const onError = (error) => {
-    connection.alive = false;
-    reject(error);
-  };
-  connection.on('timeout', onTimeout).on('close', onClose).on('error', onError);
-
-  return { onTimeout, onClose, onError };
-};
-
-/**
- * Removes callbacks attached to a connection by attachCallbacks
- *
- * @param {Connection} connection A Thrift connection
- * @param {Object} callbacks The returned { onTimeout, onClose, onError } from attachCallbacks
- */
-const detachCallbacks = (connection, { onTimeout, onClose, onError }) => {
-  connection
-    .removeListener('timeout', onTimeout)
-    .removeListener('close', onClose)
-    .removeListener('error', onError);
-};
-
 /* Constructors */
 
 /**
@@ -59,21 +18,20 @@ const detachCallbacks = (connection, { onTimeout, onClose, onError }) => {
  * @return {Promise} Resolves to an open connection or an error
  */
 const createThriftConnection = (thriftOptions) => {
-  let connection, callbacks;
   return new Promise((resolve, reject) => {
     const { host, port } = thriftOptions;
-    connection = Thrift.createConnection(host, port, thriftOptions);
+    const connection = Thrift.createConnection(host, port, thriftOptions);
     connection.alive = false; // add a property for validation purposes
-    callbacks = attachCallbacks(connection, reject);
-    connection.on('connect', resolve);
-  }).then(() => {
-    detachCallbacks(connection, callbacks);
-    connection.connection.setKeepAlive(true); // socket manipulation
-    connection.alive = true; // state tracking
-    return connection;
-  }).catch((error) => {
-    detachCallbacks(connection, callbacks);
-    throw error;
+
+    connection
+      .on('connect', () => {
+        connection.connection.setKeepAlive(true); // socket manipulation
+        connection.alive = true;
+        resolve(connection);
+      })
+      .on('timeout', () => reject(new ConnectionTimeoutError()))
+      .on('close', () => reject(new ConnectionClosedError()))
+      .on('error', reject);
   });
 };
 
@@ -90,22 +48,30 @@ const createThriftConnection = (thriftOptions) => {
 const pooledRpc = (TService, rpc, pool) => (...args) => {
   return pool.acquire()
     .catch(e => Promise.reject(new AcquisitionTimeoutError(e.message)))
-    .then((connection) => {
-      let callbacks;
-      return new Promise((resolve, reject) => {
-        callbacks = attachCallbacks(connection, reject);
-        const client = Thrift.createClient(TService, connection);
-        resolve(client[rpc](...args));
-      }).then((response) => {
-        detachCallbacks(connection, callbacks);
+    .then(connection => new Promise((resolve, reject) => {
+      const onTimeout = () => {
+        connection.alive = false;
+        reject(new ConnectionTimeoutError());
+      };
+      const onClose = () => {
+        connection.alive = false;
+        reject(new ConnectionClosedError());
+      };
+      const onError = (error) => {
+        connection.alive = false;
+        reject(error);
+      };
+      connection.on('timeout', onTimeout).on('close', onClose).on('error', onError);
+
+      const client = Thrift.createClient(TService, connection);
+      const response = client[rpc](...args).finally(() => {
+        connection.removeListener('timeout', onTimeout);
+        connection.removeListener('close', onClose);
+        connection.removeListener('error', onError);
         pool.release(connection);
-        return response;
-      }).catch((error) => {
-        detachCallbacks(connection, callbacks);
-        pool.release(connection);
-        throw error;
       });
-    });
+      resolve(response);
+    }));
 };
 
 /* Default options */
